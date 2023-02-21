5436f675f285d23234bd71b73cc53fda204cf79d
eMobilis <elikibetael4@gmail.com>
2023-02-21 10:38:35 +0300

    added working forms

--- djangoForms/forms.py
+++ djangoForms/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
+
 class user_reg(forms.Form):
-    name =forms.CharField(max_length=100)
-    email =forms.CharField(max_length=100)
-    password =forms.CharField()
-
-class user_login(forms.Form)
-    username =forms.CharField(max_length=100)
-    userpassword =forms.CharField()
+    name = forms.CharField(max_length=100)
+    email = forms.CharField(max_length=100)
+    password = forms.CharField()
 
 
+class user_login(forms.Form):
+    username = forms.CharField(max_length=100)
+    user_password = forms.CharField()
